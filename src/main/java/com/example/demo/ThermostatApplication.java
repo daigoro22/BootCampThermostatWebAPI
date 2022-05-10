47bfc84a1507cf3770a9032e2f189a7c48960a88
daigoro22 <abe.daiboro.sb@tut.jp>
2022-05-10 13:14:55 +0900

    Add FrontEnd
    フロントエンド用のコントローラとHTMLファイルを追加

--- src/main/java/com/example/demo/ThermostatApplication.java
+++ src/main/java/com/example/demo/ThermostatApplication.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
+@Configuration
 public class ThermostatApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/demo/controllers/FrontEndController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controllers;
+
+import com.example.demo.services.ThermoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/temperature")
+public class FrontEndController {
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        model.addAttribute("message", "Hello");
+        return "list";
+    }
+}
